ffd87b55b0a70501a226ec04458fec1ee5c89b39
Emilio Medina Martinez <emilio.medinamartinez@gmail.com>
2024-08-20 21:07:18 -0300

    Adding to the new cs project an interface and its implementation, that will be injected

--- DIPropertyInjection/DISampleServices/Interfaces/ISampleService.cs
+++ DIPropertyInjection/DISampleServices/Interfaces/ISampleService.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DISampleServices
+namespace DISampleServices.Interfaces
 {
-    public class Class1
+    public interface ISampleService
     {
+        int GetNumber();
     }
 }
